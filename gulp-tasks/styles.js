df472a74692e8abeb3d4f07540bf9e350d7def5c
Ramil <example@gmail.com>
2024-04-18 12:10:56 +0300

    edit settings

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -1,73 +1,52 @@
-'use strict';
+"use strict";
 
-import { paths } from '../gulpfile.babel';
-import gulp from 'gulp';
-import gulpif from 'gulp-if';
-import rename from 'gulp-rename';
-import mincss from 'gulp-clean-css';
-import groupmedia from 'gulp-group-css-media-queries';
-import sourcemaps from 'gulp-sourcemaps';
-import plumber from 'gulp-plumber';
-import browsersync from 'browser-sync';
-import debug from 'gulp-debug';
-import yargs from 'yargs';
-import postcss from 'gulp-postcss';
+import { paths } from "../gulpfile.babel";
+import gulp from "gulp";
+import gulpif from "gulp-if";
+import rename from "gulp-rename";
+import dartsass from "sass";
+import gulpsass from "gulp-sass";
+import mincss from "gulp-clean-css";
+import sourcemaps from "gulp-sourcemaps";
+import plumber from "gulp-plumber";
+import browsersync from "browser-sync";
+import debug from "gulp-debug";
+import yargs from "yargs";
 
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
-
+const sass = gulpsass(dartsass);
 const argv = yargs.argv,
-  production = !!argv.production;
+    production = !!argv.production;
 
-gulp.task('styles', () => {
-  return gulp
-    .src(paths.styles.src)
-    .pipe(gulpif(!production, sourcemaps.init()))
-    .pipe(plumber())
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(groupmedia())
-    .pipe(postcss())
-    .pipe(
-      gulpif(
-        production,
-        mincss({
-          compatibility: 'ie8',
-          level: {
-            1: {
-              specialComments: 0,
-              removeEmpty: true,
-              removeWhitespace: true,
-            },
-            2: {
-              mergeMedia: true,
-              removeEmpty: true,
-              removeDuplicateFontRules: true,
-              removeDuplicateMediaBlocks: true,
-              removeDuplicateRules: true,
-              removeUnusedAtRules: false,
-            },
-          },
-        }),
-      ),
-    )
-    .pipe(
-      gulpif(
-        production,
-        rename({
-          suffix: '.min',
-        }),
-      ),
-    )
-    .pipe(plumber.stop())
-    .pipe(gulpif(!production, sourcemaps.write('./maps/')))
-    .pipe(gulp.dest(paths.styles.dist))
-    .pipe(
-      debug({
-        title: 'CSS files',
-      }),
-    )
-    .pipe(browsersync.stream());
+gulp.task("styles", () => {
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!production, sourcemaps.init()))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulpif(production, mincss({
+            compatibility: "ie8", level: {
+                1: {
+                    specialComments: 0,
+                    removeEmpty: true,
+                    removeWhitespace: true
+                },
+                2: {
+                    mergeMedia: true,
+                    removeEmpty: true,
+                    removeDuplicateFontRules: true,
+                    removeDuplicateMediaBlocks: true,
+                    removeDuplicateRules: true,
+                    removeUnusedAtRules: false
+                }
+            }
+        })))
+        .pipe(gulpif(production, rename({
+            suffix: ".min"
+        })))
+        .pipe(plumber.stop())
+        .pipe(gulpif(!production, sourcemaps.write(".")))
+        .pipe(gulp.dest(paths.styles.dist))
+        .pipe(debug({
+            "title": "CSS files"
+        }))
+        .on("end", browsersync.reload);
 });
--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -1,3 +1,2 @@
 import '../../node_modules/jquery/dist/jquery.min.js';
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../../node_modules/swiper/swiper-bundle.js';
