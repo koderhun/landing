e450e430fa253184fcc0bad131e0f593dda6f0dd
Ramil <koderhanov@gmail.com>
2024-05-22 10:11:05 +0300

    ok

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,8 @@ console.log('Hello World')
 $('.burger, .momenu__close').on('click', () => {
   $('.momenu').toggleClass('active')
 })
+
+const swiper = new Swiper(".ncard-slider", {
+  slidesPerView: 6,
+  spaceBetween: 6,
+});
