5ed5a083cf9ccd222e4a035cfb440e1b1868b593
Ramil <koderhanov@gmail.com>
2024-05-22 19:11:37 +0300

    ok

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -23,4 +23,25 @@ $(function(){
       }
     }
   });
+
+  const gslider = new Swiper(".gslider", {
+    slidesPerView: 1,
+    spaceBetween: 6,
+    breakpoints: {
+      1440: {
+        slidesPerView: 6
+      },
+
+      1024: {
+        slidesPerView: 4
+      },
+
+      680: {
+        slidesPerView: 3
+      },
+      480: {
+        slidesPerView: 2
+      }
+    }
+  });
 });
